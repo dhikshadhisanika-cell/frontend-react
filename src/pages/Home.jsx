9b23eacc9cb0c6e660f0d10b369184ac41aab596
Dhiksha <dhikshadhisanika@gmail.com>
2026-07-04 13:04:38 +0530

    Initial React project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import Card from "../components/Card";
+
+function Home() {
+  const [favorite, setFavorite] = useState(false);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "30px" }}>
+      <h1>Home Page</h1>
+      <p>Welcome to my React App</p>
+
+      <button onClick={() => setFavorite(!favorite)}>
+        {favorite ? "❤️ Favorited" : "🤍 Add to Favorite"}
+      </button>
+
+      <Card title="HTML" description="This is HTML Card" />
+      <Card title="CSS" description="This is CSS Card" />
+      <Card title="React" description="This is React Card" />
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/ProjectDetail.jsx
@@ -0,0 +1,10 @@
+function ProjectDetail() {
+  return (
+    <div>
+      <h1>Project Detail Page</h1>
+      <p>This is the Project Detail page.</p>
+    </div>
+  );
+}
+
+export default ProjectDetail;
